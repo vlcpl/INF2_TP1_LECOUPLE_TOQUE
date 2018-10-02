fc11f5029d557d49b0f3c73a828bb166c65a1c66
vlcpl <valentin.lecouple@gmail.com>
2018-10-02 14:27:03 +0200

    Ajout de commentaires

--- src/java/beans/Account.java
+++ src/java/beans/Account.java
@@ -8,20 +8,39 @@ package beans;
 import javax.persistence.*;
 
 /**
- *
+ * Classe représentant un compte bancaire
  * @author Valentin LECOUPLE & Yann Toqué
  */
 @Entity
 public class Account {
+    /**
+     * Numéro du compte. PK de la table
+     */
     @Id
     @Column(nullable = false, length = 11)
     private String accountNumber;
+    
+    /**
+     * Label du compte
+     */
     @Column(nullable = false)
     private String label;
+    
+    /**
+     * IBAN du compte. Maximum de 27 caractères
+     */
     @Column(nullable = false, length = 27)
     private String IBAN;
+    
+    /**
+     * Agence à laquelle le compte est lié
+     */
     @Column(nullable = false)
     private BankBranch bankBranch;
+    
+    /**
+     * Somme d'argent présente sur le compte
+     */
     @Column(nullable = false)
     private double totalMoney;
 }
--- src/java/beans/Client.java
+++ src/java/beans/Client.java
@@ -8,18 +8,34 @@ package beans;
 import javax.persistence.*;
 
 /**
- *
+ * Classe représentant un client de la banque
  * @author Valentin LECOUPLE & Yann Toqué
  */
 @Entity
 public class Client{
+    
+    /**
+     * Numéro du client. PK de la table
+     */
     @Id
     @Column(nullable = false, length = 8)
     private String clientNumber;
+    
+    /**
+     * Nom de fammilee du client
+     */
     @Column(nullable = false)
     private String lastName;
+    
+    /**
+     * Prénom du client
+     */
     @Column(nullable = false)
     private String firstName;
+    
+    /**
+     * Date de naissance du client
+     */
     @Column(nullable = false)
     @Temporal(TemporalType.DATE)
     private String birthDate;
